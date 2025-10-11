78ab060b28a9da14397b02891f3bbec3dd0519b4
Mr White <wicho22gonzalez@gmail.com>
2025-10-10 20:52:32 -0600

    addres por cologne en dto Customer

--- src/main/java/com/example/proyecto/dto/CustomerResponse.java
+++ src/main/java/com/example/proyecto/dto/CustomerResponse.java
@@ -4,14 +4,13 @@ import lombok.Builder;
 import lombok.Value;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Value
 @Builder
 public class CustomerResponse {
     private Integer idCustomer;
     private String name;
-    private String address;
+    private String cologne;
     private String phone;
     private LocalDate birthDate;
     private Boolean medicalCondition;
