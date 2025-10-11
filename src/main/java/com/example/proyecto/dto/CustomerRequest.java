91c3a7c14477a0d9b4ab5551d2b9120f9d59c131
Mr White <wicho22gonzalez@gmail.com>
2025-10-10 20:58:36 -0600

    otro address por cologne

--- src/main/java/com/example/proyecto/dto/CustomerRequest.java
+++ src/main/java/com/example/proyecto/dto/CustomerRequest.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 @Data
 public class CustomerRequest {
     @NotBlank private String name;
-    @NotBlank private String address;
+    @NotBlank private String cologne;
     @Pattern(regexp = "\\d{10}") private String phone;
     @NotNull private LocalDate birthDate;
     @NotNull private Boolean medicalCondition;
