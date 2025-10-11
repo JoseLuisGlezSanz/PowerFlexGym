9e9c49ef7555821764c1ded245c2aa6c33112ed7
Mr White <wicho22gonzalez@gmail.com>
2025-10-11 06:39:44 -0600

    Tercera prueba

--- src/main/java/com/example/proyecto/model/CustomerMembership.java
+++ src/main/java/com/example/proyecto/model/CustomerMembership.java
@@ -14,13 +14,17 @@ import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "customers_memberships")
 @IdClass(CustomerMembershipId.class)
